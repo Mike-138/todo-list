0f7196208ab4c061f303af0c0ee5563bde6312d7
Michael <mikemcd.bbs@gmail.com>
2023-10-06 17:17:00 -0400

    Populate project module

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,3 +1,22 @@
 const project = (...items) => {
-    
-}
+
+    const _todoList = items;
+
+    const getTodoList = () => _todoList;
+
+    const addToTodoList = (item) => {
+        _todoList.push(item);
+    };
+
+    const removeFromTodoList = (item) => {
+        const index = _todoList.indexOf(item);
+        _todoList.splice(index, 1);
+    };
+
+    return {
+        getTodoList,
+        addToTodoList,
+        removeFromTodoList
+    };
+
+};
--- src/modules/todo.js
+++ src/modules/todo.js
@@ -4,31 +4,31 @@ const todoItem = (title, description, dueDate, priority, notes) => {
 
     const setTitle = (string) => {
         title = string;
-    }
+    };
 
     const getDescription = () => description;
 
     const setDescription = (string) => {
         description = string;
-    }
+    };
 
     const getDueDate = () => dueDate;
 
     const setDueDate = (date) => {
         dueDate = date;
-    }
+    };
 
     const getPriority = () => priority;
 
     const setPriority = (value) => {
         priority = value;
-    }
+    };
 
     const getNotes = () => notes;
 
     const setNotes = (string) => {
         notes = string;
-    }
+    };
 
     return {
         getTitle,
@@ -41,7 +41,8 @@ const todoItem = (title, description, dueDate, priority, notes) => {
         setPriority,
         getNotes,
         setNotes
-    }
-}
+    };
+    
+};
 
 export default todoItem;
