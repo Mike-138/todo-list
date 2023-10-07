d559a10bddcbb44a5f285cc975528631c0eddb48
Michael <mikemcd.bbs@gmail.com>
2023-10-07 16:05:36 -0400

    Initialize project creation modules

---
+++ src/modules/create_project_button.js
@@ -0,0 +1,5 @@
+import project from "./project_object";
+
+const buttonContainer = document.createElement("div");
+const projectButton = document.createElement("button");
+const buttonIcon = document.createElement("img");
