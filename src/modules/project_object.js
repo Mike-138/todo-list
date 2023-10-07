afc5e9febd626d85cc7c5941d4080377e46d4881
Michael <mikemcd.bbs@gmail.com>
2023-10-07 15:07:02 -0400

    Update project object properties

--- src/modules/project_object.js
+++ src/modules/project_object.js
@@ -1,5 +1,35 @@
-const project = (...items) => {
+const project = (title, description, dueDate, priority, notes, ...items) => {
 
+    const getTitle = () => title;
+
+    const setTitle = (string) => {
+        title = string;
+    };
+
+    const getDescription = () => description;
+
+    const setDescription = (string) => {
+        description = string;
+    };
+
+    const getDueDate = () => dueDate;
+
+    const setDueDate = (date) => {
+        dueDate = date;
+    };
+
+    const getPriority = () => priority;
+
+    const setPriority = (value) => {
+        priority = value;
+    };
+
+    const getNotes = () => notes;
+
+    const setNotes = (string) => {
+        notes = string;
+    };
+    
     const _todoList = items;
 
     const getTodoList = () => _todoList;
@@ -14,6 +44,16 @@ const project = (...items) => {
     };
 
     return {
+        getTitle,
+        setTitle,
+        getDescription,
+        setDescription,
+        getDueDate,
+        setDueDate,
+        getPriority,
+        setPriority,
+        getNotes,
+        setNotes,
         getTodoList,
         addToTodoList,
         removeFromTodoList
