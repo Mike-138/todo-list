7ae65417525b4c1e887a5d220193d28f3594325f
Michael <mikemcd.bbs@gmail.com>
2023-10-08 16:43:39 -0400

    Add new project stacking

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import project from "./modules/project_object";
 import todoModal from "./modules/modals/todo_modal";
 import projectModal from "./modules/modals/project_modal";
 
+const renderBody = (elements) => {
+    const stack = elements.slice().reverse();
+    document.body.replaceChildren(...stack);
+}
+
+const bodyElements = [];
+
 const addProjectButton = (() => {
     const button = document.createElement("button");
     button.classList.add("add-project-button");
@@ -14,8 +21,37 @@ const addProjectButton = (() => {
     return button;
 })();
 
-document.body.append(projectModal, addProjectButton);
+bodyElements.push(projectModal);
+bodyElements.push(addProjectButton);
+
+renderBody(bodyElements);
 
 addProjectButton.addEventListener("click", () => {
     projectModal.showModal();
+})
+
+projectModal.addEventListener("submit", () => {
+    const createProject = () => {
+        const container = document.createElement("div");
+        const title = document.createElement("h1");
+        title.textContent = projectModal.information.title;
+        const description = document.createElement("h2");
+        description.textContent = projectModal.information.description;
+        const dueDate = document.createElement("h3");
+        dueDate.textContent = projectModal.information.dueDate;
+        const priority = document.createElement("h4");
+        priority.textContent = projectModal.information.priority;
+        const notes = document.createElement("p");
+        notes.textContent = projectModal.information.notes;
+        container.append(
+            title,
+            description,
+            dueDate,
+            priority,
+            notes
+        )
+        return container;
+    }
+    bodyElements.push(createProject());
+    renderBody(bodyElements);
 })
