88a6cc4368b790c2ee5c34d00791a2d8f454ce80
Michael <mikemcd.bbs@gmail.com>
2023-10-07 23:15:54 -0400

    Create add project button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import "./styles/style.css";
+import plusIcon from "./assets/icons/plus.svg";
 import todoItem from "./modules/todo_object";
 import project from "./modules/project_object";
 import todoModal from "./modules/modals/todo_modal";
 import projectModal from "./modules/modals/project_modal";
 
-document.body.append(projectModal);
+const addProjectButton = (() => {
+    const button = document.createElement("button");
+    button.classList.add("add-project-button");
+    const image = new Image();
+    image.src = plusIcon;
+    button.appendChild(image);
+    return button;
+})();
+
+document.body.append(projectModal, addProjectButton);
