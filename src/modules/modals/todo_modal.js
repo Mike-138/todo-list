4b7127b03e7ff8859277ed80f379dcbf1e816a5d
Michael <mikemcd.bbs@gmail.com>
2023-10-10 15:19:20 -0400

    Redesign todo modal module to match new modal template

--- src/modules/modals/todo_modal.js
+++ src/modules/modals/todo_modal.js
@@ -1,5 +1,15 @@
 import modalFactory from "./modal_template";
 
-const todoModal = modalFactory("todoDialog", "todoTitle", "todoDesc", "todoDueDate", "todoPriority", "todoNotes", "todoConfirmButton");
+const createTodoModal = modalFactory("todoDialog");
+
+const todoModal = (createTodoModal
+    .makeHeader("h1", "Create a task!")
+    .makeTitleField("Title", "todoTitle")
+    .makeDescriptionField("Description", "todoDescription")
+    .makeDueDateField("Due", "todoDueDate")
+    .makePriorityField("Priority", "todoPriority")
+    .makeNotesField("Notes", "todoNotes")
+    .build()
+    );
 
 export default todoModal;
